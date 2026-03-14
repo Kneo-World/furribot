17bb660fd8bf2aa4afc852ecfd80eba45c534cd5
Kneo-World <pelukhnoillia@gmail.com>
2026-03-14 22:20:41 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import random
+import re
 from datetime import datetime, timedelta
 
 MOODS = ["happy", "sleepy", "chaotic", "aggressive", "cute"]
@@ -13,6 +14,12 @@ def cooldown_check(last_time: str, seconds: int = 86400) -> bool:
     last = datetime.fromisoformat(last_time)
     return datetime.now() - last > timedelta(seconds=seconds)
 
+def escape_markdown(text: str) -> str:
+    """Экранирует специальные символы для MarkdownV2"""
+    # Символы: _ * [ ] ( ) ~ ` > # + - = | { } . !
+    # Дефис ставим в конец, чтобы избежать диапазона в regex
+    return re.sub(r'([_*\[\]()~`>#+=|{}.!-])', r'\\\1', str(text))
+
 def format_profile(user_data: tuple, profile_data: tuple, fursona_data: tuple) -> str:
     """Форматирует профиль для вывода (MarkdownV2)"""
     if not user_data:
@@ -21,9 +28,8 @@ def format_profile(user_data: tuple, profile_data: tuple, fursona_data: tuple) -
     bio, tags = profile_data[1], profile_data[2] if profile_data else ("", "")
     species, color, personality, interests = fursona_data if fursona_data else ("не указан", "не указан", "не указан", "не указан")
 
-    # Экранируем специальные символы для MarkdownV2
     def esc(s):
-        return str(s).replace('_', '\\_').replace('*', '\\*').replace('[', '\\[').replace(']', '\\]').replace('(', '\\(').replace(')', '\\)').replace('~', '\\~').replace('`', '\\`').replace('>', '\\>').replace('#', '\\#').replace('+', '\\+').replace('-', '\\-').replace('=', '\\=').replace('|', '\\|').replace('{', '\\{').replace('}', '\\}').replace('.', '\\.').replace('!', '\\!')
+        return escape_markdown(s)
 
     return (
         f"🐾 **Профиль {esc(username)}**\n"
